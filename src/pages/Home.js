42866bba8273947dcaab0389faf4607c0a9a8992
jarekTerejko <j.terejko@gmail.com>
2021-04-08 04:41:52 +0200

    add Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import BuildingProcess from "../components/BuldingProcess/BuildingProcess";
+import DesignsLinks from "../components/DesignsLinks/DesignsLinks";
+import Hero from "../components/Hero/Hero";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <DesignsLinks />
+      <BuildingProcess />
+    </>
+  );
+};
+
+export default Home;
