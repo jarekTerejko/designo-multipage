8594c74ab04a08c8b43ef94e0fd4b84aaac4f513
jarekTerejko <j.terejko@gmail.com>
2021-04-09 07:08:51 +0200

    add WebDesign page

---
+++ src/pages/WebDesign.js
@@ -0,0 +1,12 @@
+import HeaderDesign from "../components/HeaderDesign/HeaderDesign";
+
+const WebDesign = () => {
+  return (
+    <HeaderDesign
+      heading="Web Design"
+      descriptionText="We build websites that serve as powerful marketing tools and bring memorable brand experiences."
+    />
+  );
+};
+
+export default WebDesign;
