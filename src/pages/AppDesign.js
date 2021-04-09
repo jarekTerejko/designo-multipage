026d78e6c21dbdd8adaeb5e39bf69be00a6e79e8
jarekTerejko <j.terejko@gmail.com>
2021-04-09 07:08:14 +0200

    add AppDesign page

---
+++ src/pages/AppDesign.js
@@ -0,0 +1,12 @@
+import HeaderDesign from "../components/HeaderDesign/HeaderDesign";
+
+const AppDesign = () => {
+  return (
+    <HeaderDesign
+      heading="App Design"
+      descriptionText=" Our mobile designs bring intuitive digital solutions to your customers right at their fingertips."
+    />
+  );
+};
+
+export default AppDesign;
