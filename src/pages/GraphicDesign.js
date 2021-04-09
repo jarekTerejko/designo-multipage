6ddcd50ee8c540df3d7b6cf85c850f127448aa6b
jarekTerejko <j.terejko@gmail.com>
2021-04-09 07:08:29 +0200

    add GraphicDesign page

---
+++ src/pages/GraphicDesign.js
@@ -0,0 +1,12 @@
+import HeaderDesign from "../components/HeaderDesign/HeaderDesign";
+
+const GraphicDesign = () => {
+  return (
+    <HeaderDesign
+      heading="Graphic Design"
+      descriptionText=" We deliver eye-catching branding materials that are tailored to meet your business objectives."
+    />
+  );
+};
+
+export default GraphicDesign;
