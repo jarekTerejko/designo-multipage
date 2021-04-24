8f1f7f2d129f801a3a5dd9a25bdde47a9e7df4b4
jarekTerejko <j.terejko@gmail.com>
2021-04-24 06:23:31 +0200

    fix width for submit button

--- src/components/ButtonPrimary/ButtonPrimaryElement.js
+++ src/components/ButtonPrimary/ButtonPrimaryElement.js
@@ -9,6 +9,12 @@ export const ButtonPrimaryR = styled(Link)`
   text-transform: uppercase;
   font-weight: 500;
   letter-spacing: 1px;
+  border: none;
+  min-width: 15.2rem;
+  text-align: center;
+  font-family: var(--font-family-sans);
+  align-self: ${(props) => (props.submitBtn ? "flex-end" : "")};
+  font-size: 1.6rem;
 
   transition: background-color var(--transition), color var(--transition);
 
