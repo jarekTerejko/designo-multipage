30a9ea4960e09400caa68ac1ce364de09b0b8a15
jarekTerejko <j.terejko@gmail.com>
2021-04-24 06:16:46 +0200

    add ContactBanner display logic

--- src/components/ContactBanner/ContactBanner.js
+++ src/components/ContactBanner/ContactBanner.js
@@ -8,33 +8,38 @@ import {
   ContactBannerWrapper,
 } from "./ContactBannerElements";
 import FiveCirclesBg from "../../images/shared/desktop/bg-pattern-call-to-action.svg";
+import { useLocation } from "react-router";
 
 const ContactBanner = () => {
-  return (
-    <ContactBannerWrapper>
-      <ContainerEl>
-        <ContactBannerInnerWrapper fiveCirclesBg={FiveCirclesBg}>
-          <ContactBannerTextWrapper>
-            <Heading
-              as="h2"
-              style={{
-                maxWidth: "400px",
-                marginBottom: "10px",
-                fontSize: "42px",
-              }}
-            >
-              Let's talk about your project
-            </Heading>
-            <DescriptionTextLight style={{ maxWidth: "480px" }}>
-              Ready to take it to the next level? Contact us today and find out
-              how our expertise can help your business grow.
-            </DescriptionTextLight>
-          </ContactBannerTextWrapper>
-          <ButtonPrimaryR to="/contact">get in touch</ButtonPrimaryR>
-        </ContactBannerInnerWrapper>
-      </ContainerEl>
-    </ContactBannerWrapper>
-  );
+  const location = useLocation();
+
+  if (location.pathname !== "/contact") {
+    return (
+      <ContactBannerWrapper>
+        <ContainerEl>
+          <ContactBannerInnerWrapper fiveCirclesBg={FiveCirclesBg}>
+            <ContactBannerTextWrapper>
+              <Heading
+                as="h2"
+                style={{
+                  maxWidth: "400px",
+                  marginBottom: "10px",
+                  fontSize: "42px",
+                }}
+              >
+                Let's talk about your project
+              </Heading>
+              <DescriptionTextLight style={{ maxWidth: "480px" }}>
+                Ready to take it to the next level? Contact us today and find
+                out how our expertise can help your business grow.
+              </DescriptionTextLight>
+            </ContactBannerTextWrapper>
+            <ButtonPrimaryR to="/contact">get in touch</ButtonPrimaryR>
+          </ContactBannerInnerWrapper>
+        </ContainerEl>
+      </ContactBannerWrapper>
+    );
+  } else return null;
 };
 
 export default ContactBanner;
