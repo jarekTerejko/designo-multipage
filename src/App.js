b24c054aa63e2156d85121a95a3a1c4520a74666
jarekTerejko <j.terejko@gmail.com>
2021-05-04 05:44:28 +0200

    create animations variants

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router";
+import { Route, Switch, useLocation } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
@@ -8,34 +8,52 @@ import GraphicDesign from "./pages/GraphicDesign";
 import OurCompany from "./pages/OurCompany";
 import Locations from "./pages/Locations";
 import Contact from "./pages/Contact";
+import { AnimatePresence } from "framer-motion";
 
 const App = () => {
+  const location = useLocation();
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: { delay: 0.1, duration: 1 },
+    },
+    exit: {
+      opacity: 0,
+      transition: { ease: "easeInOut" },
+    },
+  };
+
   return (
     <>
       <Navbar />
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/web-design" exact>
-          <WebDesign />
-        </Route>
-        <Route path="/app-design" exact>
-          <AppDesign />
-        </Route>
-        <Route path="/graphic-design" exact>
-          <GraphicDesign />
-        </Route>
-        <Route path="/our-company" exact>
-          <OurCompany />
-        </Route>
-        <Route path="/locations" exact>
-          <Locations />
-        </Route>
-        <Route path="/contact" exact>
-          <Contact />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <Home containerVariants={containerVariants} />
+          </Route>
+          <Route path="/web-design" exact>
+            <WebDesign containerVariants={containerVariants} />
+          </Route>
+          <Route path="/app-design" exact>
+            <AppDesign containerVariants={containerVariants} />
+          </Route>
+          <Route path="/graphic-design" exact>
+            <GraphicDesign />
+          </Route>
+          <Route path="/our-company" exact>
+            <OurCompany containerVariants={containerVariants} />
+          </Route>
+          <Route path="/locations" exact>
+            <Locations containerVariants={containerVariants} />
+          </Route>
+          <Route path="/contact" exact>
+            <Contact containerVariants={containerVariants} />
+          </Route>
+        </Switch>
+      </AnimatePresence>
       <Footer />
     </>
   );
